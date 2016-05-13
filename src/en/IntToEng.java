45515db1f62f92d8c949dde754d56d117522cd26
g14949tk <g14949tk@s30447.tsuda.ac.jp>
2016-05-13 11:16:00 +0900

    ドライバー波多野 ナビゲーター田中 Stringの配列に10までの英訳を入れた testを途中まで作った

---
+++ src/en/IntToEng.java
@@ -0,0 +1,32 @@
+package en;
+import java.util.Scanner;
+
+public class IntToEng {
+	//メインメソッド
+	 public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+	        int input = sc.nextInt();
+
+	        System.out.println(translateEng(input));
+
+	    }
+	    // 数値を英訳する変換するメソッド
+	    static String translateEng(int n) {
+	    	String[] number;	    	
+	    	number = new String[]{
+	    		"zero",
+	    		"one",
+	    		"two",
+	    		"three",
+	    		"four",
+	    		"five",
+	    		"six",
+	    		"seven",
+	    		"eight",
+	    		"nine",
+	    		"ten"
+	    	};	    	
+	        return number[n];	        
+	    }    
+}
---
+++ test/junit/en/EnTest.java
@@ -0,0 +1,14 @@
+package junit.en;
+import en.IntToEng;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EnTest {
+	@Test
+	public void ゼロを入力() {
+		IntToEng ite = new IntToEng();
+		int input = 0;
+	}
+}
