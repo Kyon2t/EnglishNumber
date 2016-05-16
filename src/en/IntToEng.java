773930ccc36a9762c520190411ecc95d3d290081
g14949tk <g14949tk@m39905.tsuda.ac.jp>
2016-05-16 14:40:58 +0900

    ドライバー波多野 ナビゲーター田中 0から99までを実装した

--- src/en/IntToEng.java
+++ src/en/IntToEng.java
@@ -10,52 +10,25 @@ public class IntToEng {
 	    }
 	    // 数値を英訳する変換するメソッド
 	    public static String translateEng(int n) {
-	    	String[] number;	    	
-	    	number = new String[]{
-	    		"zero",
-	    		"one",
-	    		"two",
-	    		"three",
-	    		"four",
-	    		"five",
-	    		"six",
-	    		"seven",
-	    		"eight",
-	    		"nine",
-	    		"ten",
-	    		"eleven",
-	    		"twelve",
-	    		"thirteen",
-	    		"fourteen",
-	    		"fifteen",
-	    		"sixteen",
-	    		"seventeen",
-	    		"eighteen",
-	    		"nineteen",
-	    		"twenty",
-	    		"twenty one",
-	    		"twenty two",
-	    		"twenty three",
-	    		"twenty four",
-	    		"twenty five",
-	    		"twenty six",
-	    		"twenty seven",
-	    		"twenty eight",
-	    		"twenty nine",
-	    		"thirty",
-	    		"thirty one",
-	    		"thirty two",
-	    		"thirty three",
-	    		"thirty four",
-	    		"thirty five",
-	    		"thirty six",
-	    		"thirty seven",
-	    		"thirty eight",
-	    		"thirty nine",
-	    		"forty"
-	    	};
-	    	String ten [] = {"zero","one","two","three","four","five","six","seven","eight","nine",};
-	    	String tenTimes [] = {"ten","twenty",};
-	        return number[n];	        
+	    	String engName;
+	    	String ten [] = {"one","two","three","four","five","six","seven","eight","nine"};
+	    	String teen[] = {"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+	    	String tenTimes [] = {"ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
+	    	if(n==0) return"zero";
+	    	if(n%10==0) {
+	    		int m = n/10;
+	    		return tenTimes[m-1];
+	    	}else{
+	    		if(n<10){
+	    			return ten[n-1];
+	    		}else if(n<20){
+	    			return teen[n-11];
+	    		}else{
+	    			int huta = n/10;
+	    			int hito = n%10;
+	    			engName = tenTimes[huta-1]+ten[hito-1];
+	    			return engName;
+	    		}
+	    	}
 	    }    
 }
